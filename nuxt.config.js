9d83ac17f0fd6f211e01352b0199f4ab3069d705
IceFrost925 <liujx925@163.com>
2018-11-03 23:16:05 +0800

    init Project

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,6 +8,7 @@ module.exports = {
   */
   head: {
     title: pkg.name,
+    
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -48,7 +49,6 @@ module.exports = {
   ** Axios module configuration
   */
   axios: {
-    proxy: true
     // See https://github.com/nuxt-community/axios-module#options
   },
 
